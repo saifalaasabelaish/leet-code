a2cf179c719162976c0682a3f235229b184bc743
saifalaasabelaish <116926046+saifalaasabelaish@users.noreply.github.com>
2024-07-21 03:32:36 +0300

    Time: 41 ms (13.38%) | Memory: 11.7 MB (40.80%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -14,15 +14,12 @@ class Solution(object):
         'M': 1000
     }
         result=0
-        length = len(s)
-        i=0
-        while i < length:
-            if i + 1 < length and roman_numbers[s[i]] < roman_numbers[s[i + 1]]:
-                result+=roman_numbers[s[i+1]]-roman_numbers[s[i]]
-                i+=2
-            else :
-                result+=roman_numbers[s[i]]
-                i+=1
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_numbers[s[i]] < roman_numbers[s[i + 1]]:
+                result -= roman_numbers[s[i]]
+            else:
+                result += roman_numbers[s[i]]
+                
         return result            
 
         
