94ceb8d75b2cf6c5cac6b411e1bd83f43e7edc5b
saifalaasabelaish <116926046+saifalaasabelaish@users.noreply.github.com>
2024-08-05 01:43:08 +0300

    Time: 19 ms (37.72%) | Memory: 11.8 MB (8.36%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -10,7 +10,8 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-
+        if not head:
+            return head
         dummy = ListNode(0)
         dummy.next = head
         tracker = dummy
