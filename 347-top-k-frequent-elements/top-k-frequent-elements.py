6225730971f703be1896e86b60e53181eacde205
saifalaasabelaish <116926046+saifalaasabelaish@users.noreply.github.com>
2024-08-06 00:53:54 +0300

    Time: 83 ms (25.04%) | Memory: 14.8 MB (86.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,7 +5,7 @@ class Solution:
         frequencies = list(count_map.values())
         frequencies.sort(reverse=True)
         
-        top_k_frequencies = frequencies[0:k]
+        top_k_frequencies = frequencies[:k]
         
         result = []
         for i, j in count_map.items():
