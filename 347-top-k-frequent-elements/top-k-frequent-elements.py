590c2e03d847a7fd7f4b8f974b5118dd2ad7799d
saifalaasabelaish <116926046+saifalaasabelaish@users.noreply.github.com>
2024-08-06 00:53:00 +0300

    Time: 79 ms (37.14%) | Memory: 14.9 MB (86.71%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, arr, k):
+        count_map = Counter(arr)
+        
+        frequencies = list(count_map.values())
+        frequencies.sort(reverse=True)
+        
+        top_k_frequencies = frequencies[0:k]
+        
+        result = []
+        for i, j in count_map.items():
+            if j in top_k_frequencies:
+                result.append(i)
+        
+        return result
