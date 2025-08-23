19512638c3270bbfc09b2e62bb1647e6d744931f
Saif sabelaish <116926046+saifalaasabelaish@users.noreply.github.com>
2025-08-23 17:05:32 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def climbStairs(self, n):
-        if n == 1:
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1:
             return 1
-        if n == 2:
+        if n==2:
             return 2
-        a, b = 1, 2
-        for i in range(3, n + 1):
-            a, b = b, a + b
-        return b
+                
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+
+        return dp[n]    
